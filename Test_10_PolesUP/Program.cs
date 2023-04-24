3c650fea65bc90051deeecfee54714cfa96de056
37533 <37533@LAPTOP-GAM2DSJ2>
2023-04-24 21:17:07 +0300

    Добавьте файлы проекта.

---
+++ Test_10_PolesUP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Test_10_PolesUP
+{
+    class Program
+    {
+        public static int Sum(int a, int r, int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += a + i * r;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(2, 3, 5));
+        }
+    }
+}
